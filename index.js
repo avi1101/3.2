60e7ba4ba52f562a6974b74027e5ac8b46056e86
elad <eladc5492@gmail.com>
2019-05-20 23:39:29 +0300

    save and view done

--- index.js
+++ index.js
@@ -74,20 +74,40 @@ app.post("/save", (req, res)=>{
     var user = req.body.username;
     var poi = req.body.POIid;
     console.log("user: "+user+"\npoi: "+poi);
-    var results = DButilsAzure.execQuery("INSERT INTO user_poi (POIID, username) VALUES(\'"+poi+"\',\'"+user+"\')");
-    results.then(function(result){
-        //console.log(result.toString());
-        // var mySubString = result.toString().substring(
-        //     result.toString().indexOf("\'") + 1,
-        //     result.toString().lastIndexOf("\'")
-        // );
-        // mySubString = mySubString +','+ poi;
-        // //console.log(mySubString);
-        // DButilsAzure.execQuery("UPDATE users SET POIs=\'"+mySubString+"\' WHERE username=\'"+user+"\'");
-        // res.status(200).send(result);
-        console.log("###########################");
-        console.log(result);
-        console.log(result.data);
+    var results = DButilsAzure.execQuery("INSERT INTO user_poi (POIID, username) VALUES(\'" + poi + "\',\'" + user + "\')");
+    results.then(function (result) {
+        res.status(200).send("Point of Interest was registered!");
+    }).catch(function(error) {
+        res.status(400).send("Point of Interest already registered for the current user");
     });
+});
 
+/**
+ * delete a POI to a user
+ * @param = JSON ({username, POI id})
+ */
+app.delete("/delete/:user/:POIid", (req,res) => {
+    var user = req.params.user;
+    var poi = req.params.POIid;
+    var results = DButilsAzure.execQuery("DELETE FROM user_poi WHERE POIID=\'"+poi+"\' AND username=\'"+user+"\'");
+    results.then(function (result) {
+        res.status(200).send("Point of Interest was deleted!");
+    }).catch(function(error) {
+        res.status(400).send("Point of Interest does not exist for the current user");
+    });
+});
+
+/**
+ * returns the number of viewed POIs of a user
+ * @params = user
+ */
+app.get("/viewedpois", (req, res) => {
+    var user = req.query['username'];
+    var results = DButilsAzure.execQuery("SELECT * FROM user_poi WHERE username=\'"+user+"\'");
+    Promise.all([results]).then(function(values) {
+        //console.log(values);
+        var x = values[0];
+        console.log(x.length);
+        res.status(200).send("The user "+user+" has: "+x.length+" POIs in record");
+    });
 });
