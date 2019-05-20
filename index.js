16deef7b0de1faf5a7da8e90ab18b801e6980041
Administrator <zeavielly1992@gmail.com>
2019-05-20 17:07:28 +0300

    DB UPDATE

--- index.js
+++ index.js
@@ -3,42 +3,16 @@
     username = group1101
     password = AviElad308
  */
-
-
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const DButilsAzure = require('./DButils');
 
-getUser(1)
-    .then(user => getInterests(user.id))
-    .then(interests => getLocation(interests[0]))
-    .then(location => console.log(location))
-    .catch(error => console.log(error.message));
-
-function getPOI(POIId) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("Getting POI");
-            app.get('/select', function(req, res){
-                DButilsAzure.execQuery(`SELECT $(POIID) FROM tableName`)
-                    .then(function(result){
-                        res.send(result)
-                    })
-                    .catch(function(err){
-                        console.log(err)
-                        res.send(err)
-                    })
-            })
-            resolve({ id: 1, name: "user1" });
-        }, 3000);
-    });
-}
-
-app.get("/", (req, res) => {
-    res.status(200).send("Hello World");
-    console.log("Got GET Request");
+app.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
 });
+app.use('/api/scripts/checkuser', auth);
+app.use('/api/scripts/poi', poi);
 
-app.listen(port,()=>{
-    console.log(`Listening on port ${port}`);
-});
+
+
